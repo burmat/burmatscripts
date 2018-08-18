d6dbdebab226a37977268e885d7ad4466a0aa1f4
Nathan Burchfield <n.t.burchfield@gmail.com>
2018-08-18 14:04:13 -0400

    fixes

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ This is just a collection of scripts and code. Some of it might be helpful for v
 
 [https://burmat.gitbook.io/security/]
 
-Please don't do anything stupid with anything you find here. Use it at your own risk - I'm not goint to be held responsible.
+Please don't do anything stupid with anything you find here. Use it at your own risk, as I will not take responsibility.
 
 \# burmat
--- bash/pkg_lookup.sh
+++ bash/pkg_lookup.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 #
+# get list of packages (rpm or dpkg) and dump them to a grep-able
+# format for searchsploit. og credit to chryzsh, modified by burmat)
 #
-# Instructions:
-# - Run the following one-liner on the victim:
-#
-# FILE="packages.txt"; FILEPATH="/tmp/$FILE"; /usr/bin/rpm -q -f /usr/bin/rpm >/dev/null 2>&1; if [ $? -eq 0 ]; then rpm -qa --qf "%{NAME} %{VERSION}\n" | sort -u > $FILEPATH; echo "kernel $(uname -r)" >> $FILEPATH; else dpkg -l | grep ii | awk '{print $2 " " substr($3,1)}' > $FILEPATH; echo "kernel $(uname -r)" >> $FILEPATH; fi; echo ""; echo "[>] Done. Transfer $FILEPATH to your computer and run: "; echo ""; echo "./packages_compare.sh /path/to/$FILE" echo "";
+#################
+# Instructions: #
+#################
 
-# - Copy the file it generates back to your machine (or any machine with searchsploit)
-# - Run this script, passing in the directory of the file:
-# 		hello@burmat~$ ./pkg_loookup.sh ~/packages.txt
+# 1) Run the following one-liner on the victim:
+#
+# > FILE="packages.txt"; FILEPATH="/tmp/$FILE"; /usr/bin/rpm -q -f /usr/bin/rpm >/dev/null 2>&1; if [ $? -eq 0 ]; then rpm -qa --qf "%{NAME} %{VERSION}\n" | sort -u > $FILEPATH; echo "kernel $(uname -r)" >> $FILEPATH; else dpkg -l | grep ii | awk '{print $2 " " substr($3,1)}' > $FILEPATH; echo "kernel $(uname -r)" >> $FILEPATH; fi; echo ""; echo "[>] Done. Transfer $FILEPATH to your computer and run: "; echo ""; echo "./packages_compare.sh /path/to/$FILE" echo "";
+#
+# 2) Copy the file it generates back to your machine (or any machine with searchsploit)
+# 3) Run this script, passing in the directory of the file:
+# >	 hello@burmat~$ ./pkg_loookup.sh ~/packages.txt
+#
+# 4) Wait for something juicy
+###################################
 
 if [ ! -k $1 ];  then
-
 	PACKAGES=$1;
-
 	if [ ! -f $PACKAGES ]; then
     	echo "[!] ERROR: File not found!"
     	exit 1
 	fi
-
 	# TODO: VERIFY THIS PATH
 	EXPLOITS=$(grep -F "/local/" /usr/share/exploitdb/files_exploits.csv | 
 			grep -v "windows" | cut -d, -f1-3 | tr --delete \" | 
 			awk -F, '{print $3 "\t" $2}' | awk -F' - ' '{print tolower($1) "\t" $2}');
-
 	IFS=$'\n'; 
-
 	for pkg in $(cat $PACKAGES); do 
-		## DEBUG
+		## DEBUG - see the terms that are searched
 		# echo -n "$pkg => " && echo -n $pkg | awk '{printf $1 " " substr($2,1,2)}'; echo "";
 		QUERY=$(echo -n $pkg | awk '{printf $1 " " substr($2,1,2)}')
 		echo "$EXPLOITS" | grep "$QUERY" --color
