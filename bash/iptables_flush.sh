4a849b7c725b7df66bbbaeb0687e002fdb2df776
Nathan Burchfield <n.t.burchfield@gmail.com>
2018-08-19 15:07:33 -0400

    add iptables_flush.sh

---
+++ bash/iptables_flush.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "[>] Flushing all firewall rules..."
+ipt="/sbin/iptables"
+[ ! -x "$ipt" ] && { echo "$0: \"${ipt}\" command not found."; exit 1; }
+$ipt -P INPUT ACCEPT
+$ipt -P FORWARD ACCEPT
+$ipt -P OUTPUT ACCEPT
+$ipt -F
+$ipt -X
+$ipt -t nat -F
+$ipt -t nat -X
+$ipt -t mangle -F
+$ipt -t mangle -X
+$ipt -t raw -F
+$ipt -t raw -X
