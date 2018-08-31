d9e21dbadde40345fad048b3e60a608a72846a8c
Nathan Burchfield <n.t.burchfield@gmail.com>
2018-08-31 19:29:19 -0400

    ping_scan.py added

---
+++ python/ping_scan.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+​
+import multiprocessing, subprocess, os
+​
+def pinger( job_q, results_q ):
+    DEVNULL = open(os.devnull,'w')
+    while True:
+        ip = job_q.get()
+        
+        if ip is None: break
+        
+        try:
+            subprocess.check_call(['ping','-c1',ip],
+                                  stdout=DEVNULL)
+            results_q.put(ip)
+        except:
+            pass
+​
+if __name__ == '__main__':
+    pool_size = 255
+    jobs = multiprocessing.Queue()
+    results = multiprocessing.Queue()
+    pool = [ multiprocessing.Process(target=pinger, args=(jobs,results))
+             for i in range(pool_size) ]
+    
+    for p in pool:
+        p.start()
+​
+    for i in range(1,255):
+        jobs.put('10.1.1.{0}'.format(i))
+​
+    for p in pool:
+        jobs.put(None)
+​
+    for p in pool:
+        p.join()
+​
+    while not results.empty():
+        ip = results.get()
+        print(ip)
