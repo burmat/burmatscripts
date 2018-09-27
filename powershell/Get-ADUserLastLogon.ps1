f43f1515a72f94108068ac923dfecf78748d2563
Nathan Burchfield <n.t.burchfield@gmail.com>
2018-09-27 12:40:38 -0400

    Get-AdUserLastLogon.ps1

---
+++ powershell/Get-ADUserLastLogon.ps1
@@ -0,0 +1,20 @@
+ Import-Module ActiveDirectory
+
+function Get-ADUserLastLogon([string]$userName) {
+
+    $dcs = Get-ADDomainController -Filter {Name -like "*"}
+    $time = 0
+    foreach($dc in $dcs) { 
+        $hostname = $dc.HostName
+        $user = Get-ADUser $userName | Get-ADObject -Properties lastLogon 
+        if($user.LastLogon -gt $time) {
+            $time = $user.LastLogon
+        }
+    }
+    
+    $dt = [DateTime]::FromFileTime($time)
+    Write-Host $username "last logged on at:" $dt 
+}
+
+$unames = Get-ADUser -Filter 'ObjectClass -eq "User"' | Select -Expand SamAccountName
+foreach ($uname in $unames) { Get-ADUserLastLogon($uname); } 
--- powershell/README.md
+++ powershell/README.md
@@ -4,5 +4,6 @@
 |-----------------|--------------------------------------------------------------------------------------|
 | [ftp_upload.ps1](https://github.com/burmat/burmatscripts/blob/master/powershell/ftp_upload.ps1) | FTP File Uploader with Email Notification |
 | [Get-ADHostsLastLogon.ps1](https://github.com/burmat/burmatscripts/blob/master/powershell/Get-ADHostsLastLogon.ps1) | Get a last logon datetime for every computer in AD |
+| [Get-ADUserLastLogon.ps1](https://github.com/burmat/burmatscripts/blob/master/powershell/Get-ADUserLastLogon.ps1) | Get the last logon datetime for each user account in the domain |
 |  |  |
 
