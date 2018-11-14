5c79978244acb4b45d366906774dd934417c42d3
Nathan Burchfield <n.t.burchfield@gmail.com>
2018-11-14 11:03:27 -0500

    TFS-Commit.ps1

--- powershell/README.md
+++ powershell/README.md
@@ -6,6 +6,6 @@
 | [Get-ADHostsLastLogon.ps1](https://github.com/burmat/burmatscripts/blob/master/powershell/Get-ADHostsLastLogon.ps1) | Get a last logon datetime for every computer in AD |
 | [Get-ADStaleHosts.ps1](https://github.com/burmat/burmatscripts/blob/master/powershell/Get-ADStaleHosts.ps1) | Get all computers that have not been logged into for the last 30 days |
 | [Get-ADUserLastLogon.ps1](https://github.com/burmat/burmatscripts/blob/master/powershell/Get-ADUserLastLogon.ps1) | Get the last logon datetime for each user account in the domain |
-| [TFSCommit.ps1](https://github.com/burmat/burmatscripts/blob/master/powershell/TFSCommit.ps1) | Bulk commit a project folder to a TFS project |
+| [TFS-Commit.ps1](https://github.com/burmat/burmatscripts/blob/master/powershell/TFS-Commit.ps1) | Copy code folder to workspace and commit to a TFS project |
 |  |  |
 
---
+++ powershell/TFS-Commit.ps1
@@ -0,0 +1,247 @@
+###############################################################################
+# TFS-Commit.ps1 
+# Author: burmat
+# Get-Command -Module Microsoft.TeamFoundation.PowerShell
+###############################################################################
+#
+# This script is for a special case where we have a large code repository, but 
+# no way to properly check out and make modifications. This is because the code
+# needs to be run on a particular server and compiler service to 
+# test. This makes it impossible to use revision control as designed and still
+# maintain a team of programmers.
+#
+# This script was created to pull down those working directories push the files 
+# into TFS anyway, so we can at least track changes to the project folders. This 
+# script is heavy in that it will copy everything and try to commit everything 
+# (only changes are ACTUALLY committed). Futher optimization can by done by 
+# only copying modified files and attempting to merge modified files only (TODO)
+#
+###############################################################################
+#
+# TO USE ON NEW PROJECT - FIRST RUN/COMMIT:
+#   1) Create a project in TFS
+#   2) Add the Project name to the `Merge-ProjectCode()` w/ filesystem location
+#   2) Run with the -FullCommit flag to copy all files and commit all files
+#
+###############################################################################
+#
+#   PARAMETER: -ProjectName "<PROJECT>"
+#       The name of your TFS project, mapped to directory in Merge-ProductCode()
+#
+#   PARAMETER: -Merge
+#       Auto merge project developent code without confirmation
+#
+#   PARAMETER: -Clone
+#       Auto clone TFS project to local folder without confirmation
+#
+#   PARAMETER: -FullCommit
+#       Push all code (-Add) to staging and attempt to commit (First run)
+#
+#   PARAMETER: -Alteration
+#       Simulate an alteration by adding a new file to the workspace
+#
+#   EXAMPLE - Push a new project into TFS (All files and folders)
+#       PS > ./TFS-Commit.ps1 -ProjectName "PROJECT1" -FullCommit
+#
+#   EXAMPLE - Merge without a confirmation (good for scheduled task):
+#       PS > ./TFS-Commit.ps1 -ProjectName "PROJECT1" -Clone -Merge
+#
+###############################################################################
+
+param(
+    [string]$ProjectName = "INVALID", 
+    [switch]$Merge = $false, 
+    [switch]$Clone = $false, 
+    [switch]$FullCommit = $false,
+    [switch]$Alteration = $false
+)
+
+Add-PSSnapin Microsoft.TeamFoundation.PowerShell
+
+$PROJECT = $ProjectName
+$URL = "http://coderepo.burmat.co:8080/tfs/Projects"
+$SERVERPATH = "$/$PROJECT"
+$LOCALFOLDER = "C:\TFS-REPO\$PROJECT\"
+$WORKSPACENAME = "TFS-$PROJECT"
+
+## used to simulate a change to the workspace (call `Alter-Workspace()`)
+$LOCALFILE = "$LOCALFOLDER\burmatwashere.txt"
+
+## Maps TFS Project to development code.  Copies everything to 
+## the workspaces local directory.
+function Merge-ProjectCode() {
+
+    switch ($PROJECT) {
+        "PROJECT1" { $source = "\\CODEREPO\PROJECT1\*"; break }
+        "PROJECT2" { $source = "\\CODEREPO\PROJECT2\*"; break }
+        default { $source = "INVALID"; break }  
+    }
+
+    if ($source -eq "INVALID") {
+        Exit-WithError "ProjectName provided is invalid! Please check your spelling"
+    } else {
+        Print-Debug "Merging development server code (May take a while).."
+        Copy-Item $source -Destination $LOCALFOLDER -Force -Recurse
+    }
+}
+
+## Remove the directory and the workspace if it exists. Re-create the
+## directory and map the workspace to it.
+function Initialize-Workspace() {
+
+    ## if the local directory exists, remove it
+    if (Test-Path -Path $LOCALFOLDER) {
+        Print-Debug "Deleting local folder: $LOCALFOLDER"
+        Remove-Item -Path "$LOCALFOLDER\*" -Recurse -Force | Out-Null
+        Remove-Item -Path "$LOCALFOLDER" -Recurse -Force | Out-Null
+    }
+
+    ## create the directory to house the workspace
+    New-Item -Path $LOCALFOLDER -Type Directory | Out-Null 
+
+    Try {
+        ## try to find the workspace
+        $Workspace = $VcServer.GetWorkspace($WORKSPACENAME, $env:USERNAME)
+        if ($Workspace -ne $null) {
+            Print-Debug "Workspace found, deleting it now.."
+            $VcServer.DeleteWorkspace($WORKSPACENAME, $env:USERNAME) | Out-Null
+        }
+    } Catch {
+        Print-Debug "No workspace found."
+    }
+
+    ## create the workspace, link to local directory
+    Print-Debug "Initializing workspace..."
+    $Workspace = $VcServer.CreateWorkspace($WORKSPACENAME, $env:USERNAME)
+    $Workspace.Map($SERVERPATH, $LOCALFOLDER)
+
+    ## if desired, clone the TFS Project code down to workspace:
+    if ($Clone) {
+        Print-Debug "Cloning TFS project to $LOCALFOLDER (May take a while).."
+        $Workspace.Get([Microsoft.TeamFoundation.VersionControl.Client.VersionSpec]::Latest, 
+            [Microsoft.TeamFoundation.VersionControl.Client.GetOptions]::Overwrite)
+    } else {
+        $Confirm = Read-Host "Would you like to clone a local copy of $PROJECT now? [y/N]"
+        if ($Confirm -Match "[yY]") {
+            Print-Debug "Cloning TFS project to $LOCALFOLDER (May take a while).."
+            $workspace.Get([Microsoft.TeamFoundation.VersionControl.Client.VersionSpec]::Latest, 
+            [Microsoft.TeamFoundation.VersionControl.Client.GetOptions]::Overwrite)
+        }
+    }
+}
+
+
+## Iterate all files in the directory. If this is a -FullCommit, all files
+## are added to staging with -Add. Otherwise, the files are only added to 
+## staging if they have been modified in the last 48 hours. This speeds
+## up the script dramatically and causes less confusion on -Add / -Edit
+function Commit-workspace() {
+    Try {
+        $Workspace = $VcServer.GetWorkspace($WORKSPACENAME, $env:USERNAME)
+        if ($Workspace -eq $null) {
+            Exit-WithError "Workspace not initialized properly. Please re-initialize."
+        }
+    } Catch {
+        Exit-WithError "Unable to commit, workspace doesn't exist. Please re-initialize."
+    }
+
+    Print-Debug "Adding files to staging..."
+
+    ## for every file in the workspace directory:
+    Get-ChildItem $LOCALFOLDER -Recurse | ForEach-Object { 
+
+        $file = $_.FullName
+        $isDir = Test-Path -Path $file -PathType Container
+
+        if ($isDir -eq $False -And $file.contains('/$tf/') -eq $False){
+         
+            ## if flag given, add all files to staging
+            if ($FullCommit) {
+                Add-TfsPendingChange -Add -Item "$file" | Out-Null
+            } else {
+                ## only add files to staging if modified in last 48 hours
+                if ($_.LastWriteTime -gt (Get-Date).AddDays(-2) -And $file.contains('*/$tf/*') -eq $False) {
+                    ## check if already in workspace and add if not
+                    $added = Get-TfsItemProperty -Item $file | Select -ExpandProperty IsInWorkspace
+                    if ($added) {
+                        Add-TfsPendingChange -Edit -Item $file | Out-Null
+                    } else {
+                        Add-TfsPendingChange -Add -Item "$file" | Out-Null
+                    }
+                }
+            }
+        }
+    }
+
+    $PendingChanges = $Workspace.GetPendingChanges()
+    if ($PendingChanges) {
+
+        ## for logging purposes:
+        Print-Debug "`n`nStaged files to commit:"
+        $PendingChanges | fl FileName,ChangeType,ServerItem,CreationDate
+
+        ## check in the code
+        $Author = "POWERSHELL"
+        $Comment = "*** AUTO COMMIT ***"
+        if ($FullCommit) {
+            $Comment = "Project Full Commit"
+        }
+        $PolicyOverrideInfo = New-Object Microsoft.TeamFoundation.VersionControl.Client.PolicyOverrideInfo("Auto checkin", $null)
+        $CheckinOptions = [Microsoft.TeamFoundation.VersionControl.Client.CheckinOptions]::SuppressEvent
+        $Workspace.CheckIn($PendingChanges, $Author, $Comment, $null, $null, $PolicyOverrideInfo, $CheckinOptions)
+        
+        Print-Debug "`n`n---> Check-in Completed"
+    
+        ## you can check in file by file with: 
+        ##  PS > New-TfsChangeset -Item "$LOCALFOLDER\burmatwashere.txt"" -Verbose -Override true
+
+    } else {
+        Print-Debug "No files to check in"
+    }
+}
+
+# Exit with error code 1 after logging message
+function Exit-WithError([string]$msg) { Write-Host("[#] Exiting due to error: $msg"); Exit 1; }
+
+# Log a debug message
+function Print-Debug([string]$msg) { Write-Host "[#] $msg" }
+
+# Simulate a change to the workspace
+function Alter-Workspace() { Get-Date | Out-File $LOCALFILE }
+
+
+## instatiate the tfs server
+$TfsServer = Get-TfsServer -Name $URL
+
+## instatiate the version control server
+$VcServer = $TfsServer.GetService([Microsoft.TeamFoundation.VersionControl.Client.VersionControlServer])
+
+## instatiate the project
+Try {
+    $VcProject = $VcServer.GetTeamProject($PROJECT)
+} Catch {
+    Exit-WithError "Unable to find project with that name on TFS Server."
+}
+
+## remove and re-create directory/workspace
+Initialize-Workspace
+
+if ($Alteration) {
+    ## simulate a change to the workspace
+    Alter-Workspace 
+}
+
+if ($Merge) {
+    Merge-ProjectCode
+} else {
+    $Confirm = Read-Host "Would you like to merge live $PROJECT code into this workspace? [y/N]"
+    if ($Confirm -Match "[yY]") {
+        Merge-ProjectCode
+    }
+}
+
+## attempt to commit any changes to the workspace
+Commit-Workspace
+
+Write-Host "`n------"
+Write-Host "Done."
