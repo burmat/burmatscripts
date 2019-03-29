a2322b2552ba3c73934251eecdcb3c9acdf3946e
Nathan Burchfield <n.t.burchfield@gmail.com>
2019-03-29 11:54:57 -0400

    indentation

--- powershell/ActiveSyncCleanup.ps1
+++ powershell/ActiveSyncCleanup.ps1
@@ -14,39 +14,39 @@ $devices = Get-ActiveSyncDevice -ResultSize unlimited;
 
 # iterate them:
 foreach ($device in $devices) {
-	
+
 	# get the last sync date:
 	$stats = Get-ActiveSyncDeviceStatistics -Identity $device
-	
-    # if statistics were found:
+
+	# if statistics were found:
 	if ($stats) {
-		
-        # pull out the user
-        $userAcct = ([regex]::split($device.Identity,'\/ExchangeActiveSyncDevices'))[0]
-        $userAcct = ([regex]::split($userAcct, "/"))[2] # hacked but works
 
-        # values we want to save back tot he `$device`
+		# pull out the user
+		$userAcct = ([regex]::split($device.Identity,'\/ExchangeActiveSyncDevices'))[0]
+		$userAcct = ([regex]::split($userAcct, "/"))[2] # hacked but works
+
+		# values we want to save back tot he `$device`
 		$lastAttempt = $stats.LastSyncAttemptTime
-        $lastSuccess = $stats.LastSuccessSync
+		$lastSuccess = $stats.LastSuccessSync
 
-        # if it hasn't even attempted to sync in 1 year, remove it
+		# if it hasn't even attempted to sync in 1 year, remove it
 		if ($lastAttempt -lt (Get-Date).AddDays(-365)) {
 			Write-Host "[>] REMOVING DEVICE - $userAcct [$stats.Guid] (last sync attempt: $lastAttempt)"
-            Remove-ActiveSyncDevice $device -Confirm:$False
+			Remove-ActiveSyncDevice $device -Confirm:$False
 		} else {
-            Write-Host "[#] ACTIVE DEVICE FOUND FOR $userAcct (last sync success: $lastSuccess"
+			Write-Host "[#] ACTIVE DEVICE FOUND FOR $userAcct (last sync success: $lastSuccess"
 			$device | Add-Member -MemberType NoteProperty -Name "LastSuccessSync" -Value $lastSuccess
-            $device | Add-Member -MemberType NoteProperty -Name "LastSyncAttemptTime" -Value $lastAttempt
+			$device | Add-Member -MemberType NoteProperty -Name "LastSyncAttemptTime" -Value $lastAttempt
 		}
 	} else {
-        # no stats found? If the user is in retire OU, remove device completely:
-        if ($device.Identity -Like "*/Retire/*") {
-            Write-Host "[>] REMOVING AD OBJECT: $device.Identity"
+		# no stats found? If the user is in retire OU, remove device completely:
+		if ($device.Identity -Like "*/Retire/*") {
+			Write-Host "[>] REMOVING AD OBJECT: $device.Identity"
 			## `Remove-ActiveSyncDevice` won't work if the mailbox is disconnected or user disabled
 			Remove-ADObject -Identity $device.DistinguishedName -Confirm:$False
 		} else {
-            Write-Host "[!] ORPHANED ACTIVESYNC DEVICE: $device.Identity"
-        }
+			Write-Host "[!] ORPHANED ACTIVESYNC DEVICE: $device.Identity"
+		}
 	}
 }
 
